ddc26f32cbd5cef6f1e5642c827133dafbf67050
Baleni <wang_chang_song@163.com>
2020-09-26 22:35:30 +0800

    晚安

--- src/main/java/TextGit.java
+++ src/main/java/TextGit.java
@@ -1,5 +1,7 @@
 public class TextGit {
     public static void main(String[] args) {
         System.out.println("又是美好的一天");
+        System.out.println("Good night");
+        System.out.println("see you tomorrow");
     }
 }
